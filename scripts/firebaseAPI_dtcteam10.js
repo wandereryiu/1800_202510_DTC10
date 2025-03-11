695b4510c385508b9c8c880169f75156171f525e
wandereryiu <yiyzhao1@gmail.com>
2025-03-11 04:07:29 -0700

    Implement Firebase console, complete login.html, and add Firebase UI for user login

---
+++ scripts/firebaseAPI_dtcteam10.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDCdDfFiMvz767-VoIpDefZLjAjCkYHyqg",
+    authDomain: "simplsaving-dtc10.firebaseapp.com",
+    projectId: "simplsaving-dtc10",
+    storageBucket: "simplsaving-dtc10.firebasestorage.app",
+    messagingSenderId: "472977879890",
+    appId: "1:472977879890:web:27715620995b0637e7c08b",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -8,7 +8,7 @@ function loadSkeleton() {
         if (user) {
             // If the "user" variable is not null, then someone is logged in
             // User is signed in.
-            // Do something for the user here.
+
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         } else {
