ffa42d5eda1f2f598450aaaa2084b82842ae88d0
Brandon Berge <bdberge@gmail.com>
2025-03-12 05:43:29 -0700

    Add authentication functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
 function sayHello() {
-    
+
 }
 //sayHello();
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+    }).catch((error) => {
+        // An error happened.
+    });
+}
+
+
