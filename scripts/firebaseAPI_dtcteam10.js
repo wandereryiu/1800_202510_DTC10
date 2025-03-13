6b5a2a8916fe48d528c7170d17ebf628d6c3bac2
wandereryiu <yiyzhao1@gmail.com>
2025-03-12 21:42:15 -0700

    Remove sensitive information

--- scripts/firebaseAPI_dtcteam10.js
+++ scripts/firebaseAPI_dtcteam10.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyCEoKBCyOnoCYJ9duyRBARqlsmJ7jjCK_E",
-    authDomain: "comp1800---term-project-3624d.firebaseapp.com",
-    projectId: "comp1800---term-project-3624d",
-    storageBucket: "comp1800---term-project-3624d.firebasestorage.app",
-    messagingSenderId: "1061871897541",
-    appId: "1:1061871897541:web:e00119fce99e7c8749d9ac"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: 
 };
 
 // Initialize Firebase
